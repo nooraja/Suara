9fcd18a6c1af38d2a712b3138a838676c6d116a3
Noor <muhammadnoor007@gmail.com>
2021-07-16 12:04:38 +0700

    Initial Commit

---
+++ Suara/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Suara
+//
+//  Created by Muhammad Noor on 16/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
